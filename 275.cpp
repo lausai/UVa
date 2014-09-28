da3556ad50dbc34c1c513c1f829cc20d27df60aa
lausai <lausai360@gmail.com>
2014-09-28 19:37:43 +0800

    add 275.cpp

---
+++ 275.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void foo(int a, int b)
+{
+    if (a == 0) {
+        printf(".\nThis expansion terminates.\n\n");
+        return;
+    }
+
+    vector<int> list;
+    bool repeat = true;
+    int count = 1;
+
+    printf(".");
+
+    while (find(list.begin(), list.end(), a) == list.end()) {
+        list.push_back(a);
+
+        int c = (a * 10) < b ? 0 : (a * 10) / b;
+        a = c == 0 ? a * 10 : (a * 10) % b;
+
+        if (count == 50) {
+            puts("");
+            count = 0;
+        }
+
+        printf("%d", c);
+        count++;
+
+        if (a == 0) {
+            repeat = false;
+            break;
+        }
+    }
+
+    if (repeat)
+        printf("\nThe last %d digits repeat forever.\n\n", list.size());
+    else
+        puts("\nThis expansion terminates.\n");
+}
+
+int main()
+{
+    int a, b;
+    
+    while (scanf("%d %d", &a, &b), a != 0 || b != 0) {
+        foo(a, b);
+    }
+
+	return 0;
+}
